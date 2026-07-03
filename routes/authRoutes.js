3ebe1514b51090ba695ff893b9df209e8026afe0
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-07-03 11:32:07 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@
 
 // import express from "express";
 // import bcrypt from "bcryptjs";
+
 // import generateToken from "../utils/generateToken.js";
 // import protect from "../middleware/authMiddleware.js";
 
